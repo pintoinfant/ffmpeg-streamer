d704315786a8f0142eb15d879b44aa229ba44eb0
Pinto565 <pintoinfant5650@gmail.com>
2021-12-30 18:18:40 +0000

    update files

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require('telegraf')
 const { v4: uuidv4 } = require('uuid');
 const shell = require("shelljs")
+require("dotenv").config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const ownerID = process.env.OWNER_ID
@@ -12,6 +13,7 @@ bot.command('stream', async (ctx) => {
         let link = ctx.message.text.split(" ")[1]
         let uniqueId = uuidv4()
         await ctx.reply(`Your video is now being Streamed to ${baseUrl}${uniqueId}`)
+        console.log("Video Streaming")
         shell.exec(`ffmpeg -re -i "${link}" -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv ${baseUrl}${uniqueId}`, { silent: true, async: true })
     }
 })
